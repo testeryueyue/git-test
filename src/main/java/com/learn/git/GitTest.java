1ff089e695399b98765e3be3eaefe6072b51447d
tester <tester@glo.com>
2023-03-17 16:32:50 +0800

    master five commit

--- src/main/java/com/learn/git/GitTest.java
+++ src/main/java/com/learn/git/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("hello git5 hot-fix分支第二次修改!");
         System.out.println("hello git4 hot-fix分支修改!");
         System.out.println("hello git5 master分支修改!");
+        System.out.println("hello git6 master分支第五次修改!");
     }
 }
